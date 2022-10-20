7a64c3418b0ad7158355db8ec4aa84b9b43c5c85
EKO <eko@ivu.de>
2022-10-20 13:11:12 +0200

    Create Task List component + 4 stories

---
+++ src/app/components/task-list/task-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Task } from '../../models/task.model';
+
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.css'],
+})
+export class TaskListComponent {
+  tasksInOrder: Task[] = [];
+
+  @Input() loading = false;
+
+  @Input()
+  set tasks(arr: Task[]) {
+    this.tasksInOrder = [
+      ...arr.filter((t) => t.state === 'TASK_PINNED'),
+      ...arr.filter((t) => t.state === 'TASK_INBOX'),
+    ];
+  }
+
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output()
+  onPinTask = new EventEmitter<Event>();
+
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output()
+  onArchiveTask = new EventEmitter<Event>();
+}
