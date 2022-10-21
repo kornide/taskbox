722344555892a6e62e35499520d97d4adeb4d3de
EKO <eko@ivu.de>
2022-10-21 13:37:47 +0200

    Fix: Use correct component names in spec files

--- src/app/components/task/task-list-wrapper/task-list-wrapper.component.spec.ts
+++ src/app/components/task/task-list-wrapper/task-list-wrapper.component.spec.ts
@@ -1,19 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TaskListWrapperComponent } from './task-list-wrapper.component';
 
-import { TaskListComponent } from './task-list-wrapper.component';
-
-describe('TaskListComponent', () => {
-  let component: TaskListComponent;
-  let fixture: ComponentFixture<TaskListComponent>;
+describe('TaskListWrapperComponent', () => {
+  let component: TaskListWrapperComponent;
+  let fixture: ComponentFixture<TaskListWrapperComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [TaskListComponent],
+      declarations: [TaskListWrapperComponent],
     }).compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TaskListComponent);
+    fixture = TestBed.createComponent(TaskListWrapperComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/components/task/task-list/task-list.component.spec.ts
+++ src/app/components/task/task-list/task-list.component.spec.ts
@@ -1,19 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TaskListComponent } from './task-list.component';
 
-import { PureTaskListComponent } from './task-list.component';
-
-describe('PureTaskListComponent', () => {
-  let component: PureTaskListComponent;
-  let fixture: ComponentFixture<PureTaskListComponent>;
+describe('TaskListComponent', () => {
+  let component: TaskListComponent;
+  let fixture: ComponentFixture<TaskListComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [PureTaskListComponent],
+      declarations: [TaskListComponent],
     }).compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PureTaskListComponent);
+    fixture = TestBed.createComponent(TaskListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
